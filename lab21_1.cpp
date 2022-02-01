9e248f68678fed31efb300f199355feb9b4f5281
worapit <drm.wmwork@gmail.com>
2022-02-01 20:10:24 +0700

    complete

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,23 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+#include<iostream>
+using namespace std;
 
+struct Rect{
+	double x,y,w,h;
+};
 
-
+double overlap(Rect A, Rect B){
+	double max_X = max(A.x,B.x);
+	double min_X = min(A.x+A.w,B.x+B.w);
+	double min_Y = min(A.y,B.y);
+	double max_Y = max(A.x-A.h,B.x-B.h);
+	double overlap_X = min_X-max_X;
+	double overlap_Y = min_Y-max_Y;
+	if(overlap_X <= 0 || overlap_Y <= 0)
+		return 0;
+	return overlap_X*overlap_Y;
 }
+
+
--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,54 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *A,Rect *B){
+	double XA,YA;
+	XA = A->x + A->w;
+	YA = A->y - A->h;
+	double XB,YB;
+	XB = B->x + B->w;
+	YB = B->y - B->h;
 
+    double a,b,c,d;
 
+	if(A->x == B->x){
+		a = 0;
+	}else if(A->x > B->x){
+	       a = A->x;
+	    }else{
+		     a = B->x;
+	    }
+
+	if(XA == XB){
+		b = 0;
+	}else if(XA < XB){
+		  b = XA;
+	   }else{
+		  b = XB;
+	   }
+    
+	if(A->x == B->y){
+		c = 0;
+	}else if(A->y < B->y){
+	    c = A->y;
+	}else{
+		c = B->y;
+	}
+    
+	if(XA == XB){
+	    d = 0;
+	}else if(YA < YB){
+		d = YB;
+	}else{
+		d = YA;
+	}
+    double answer;
+	if(a < b && d < c){
+		answer = (b-a)*(c-d);
+		if(answer < 0) answer = -answer;
+		return answer;
+	}else{
+		return 0;
+	}
 
 }
